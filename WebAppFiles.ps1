eda39c1a5b640c5e981b70feff88ba0377691a30
Michael Hansen <mihansen@microsoft.com>
2018-01-09 22:28:54 -0500

    Some fixes to WebAppFiles.ps1

--- WebAppFiles.ps1
+++ WebAppFiles.ps1
@@ -1,6 +1,34 @@
-function Copy-FileToWebApp($ResourceGroupName, $WebAppName, $File, $Destination)
+function Copy-FileToWebApp
 {
+    param(
+        [Parameter(Mandatory)]
+        [String]$ResourceGroupName,
+        
+        [Parameter(Mandatory)]
+        [String]$WebAppName,
+        
+        [Parameter(Mandatory)]
+        [String]$File, 
+        
+        [Parameter(Mandatory)]
+        [String]$Destination,
 
+        [Parameter(Mandatory=$false)]
+        [System.Object]$PublishingCredentials
+    )
+
+    if ([String]::IsNullOrEmpty($PublishingCredentials)) {
+        $PublishingCredentials = $(Get-WebAppPublishingCredentials -ResourceGroupName $ResourceGroupName -WebAppName $WebAppName)
+    }
+
+    $webclient = New-Object -TypeName System.Net.WebClient
+    $webclient.Credentials = New-Object System.Net.NetworkCredential($PublishingCredentials.username,$PublishingCredentials.password)
+
+    $uri = New-Object System.Uri($PublishingCredentials.url + "/$Destination")
+    $webclient.UploadFile($uri, $(Get-ChildItem $File).FullName)
+    $webclient.Dispose()
+
+    return $PublishingCredentials
 }
 
 function Get-WebAppPublishingCredentials
@@ -9,11 +37,11 @@ function Get-WebAppPublishingCredentials
         [Parameter(Mandatory)]
         [String]$ResourceGroupName,
                 
-        [Paramete(Mandatory)]
+        [Parameter(Mandatory)]
         [String]$WebAppName
     )
-    
-    $xml = [xml](Get-AzureRmWebAppPublishingProfile -Name $WebAppName -ResourceGroupName $ResourceGroupName -OutputFile null)
+
+    $xml = [xml](Get-AzureRmWebAppPublishingProfile -Name $WebAppName -ResourceGroupName $ResourceGroupName)
 
     $username = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@userName").value
     $password = $xml.SelectNodes("//publishProfile[@publishMethod=`"FTP`"]/@userPWD").value
