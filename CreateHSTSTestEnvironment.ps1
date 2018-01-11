9d5092b0c00dd8380c16edba04184b765c2560f6
Michael Hansen <mihansen@microsoft.com>
2018-01-11 11:21:10 -0500

    Added missing file copy to test environment

--- CreateHSTSTestEnvironment.ps1
+++ CreateHSTSTestEnvironment.ps1
@@ -9,6 +9,8 @@ param(
     [String]$WebAppName
 )
 
+. .\WebAppFiles.ps1
+
 $azcontext = Get-AzureRmContext
 
 if ([string]::IsNullOrEmpty($azcontext.Account)) {
@@ -23,3 +25,5 @@ $asp = New-AzureRmAppServicePlan -Name $WebAppName -Location $rg.Location -Resou
 $webApp = New-AzureRmWebApp -Name $WebAppName -Location $rg.Location -AppServicePlan $WebAppName -ResourceGroupName $rg.ResourceGroupName
 
 Write-Host "Web App Deployed: " + $webApp.DefaultHostName
+
+$creds = Copy-FileToWebApp -WebAppName $WebAppName -ResourceGroupName $rg.ResourceGroupName -File .\index.html -Destination "/index.html"
